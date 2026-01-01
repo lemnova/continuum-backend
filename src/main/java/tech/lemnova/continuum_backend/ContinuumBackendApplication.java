30ef6822c2b775bc44b72d85f7598429d863f271
lemavos <lemxvos.dev@proton.me>
2026-01-01 15:21:15 -0300

    chore: initialize Spring Boot backend

---
+++ src/main/java/tech/lemnova/continuum_backend/ContinuumBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.lemnova.continuum_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/lemnova/continuum_backend/ContinuumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lemnova.continuum_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
