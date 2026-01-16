5806ddf47780ede4794a4f80b609d72e341b2d18
lemavos <lemxvos.dev@proton.me>
2026-01-16 11:34:04 -0300

    add health controller

--- src/main/java/tech/lemnova/continuum_backend/config/SystemController.java
+++ src/main/java/tech/lemnova/continuum_backend/config/SystemController.java
@@ -4,7 +4,10 @@
  */
 package tech.lemnova.continuum_backend.config;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,20 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class SystemController {
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return """
-               <html>
-                   <head>
-                       <title>Continuum API</title>
-                       <meta charset="UTF-8">
-                       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                   </head>
-                   <body>
-                        <p1>The API is alive!. Everything is okay :)</p1><br>
-                        <a href="https://github.com/lemnova/continuum-backend">For more details click here!</a>
-                   </body>
-               </html>
-               """;
+        <html>
+            <head>
+                <title>Continuum API</title>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+                 <p1>The API is alive!. Everything is okay :)</p1><br>
+                 <a href="https://github.com/lemnova/continuum-backend">For more details click here!</a>
+            </body>
+        </html>
+        """;
+    }
+
+    @RequestMapping(value = "/health",
+            method = {RequestMethod.GET,
+            RequestMethod.HEAD})
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
     }
 }
